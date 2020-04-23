92a9f3fd5718e6ca6658ab4a118fa4ba175c84bb
Sidharth Nair <snair2103@gmail.com>
2020-04-23 14:23:38 +0530

    Debugging

--- Flask/Flask_server.py
+++ Flask/Flask_server.py
@@ -1,4 +1,3 @@
-
 import flask
 
 app = flask.Flask(__name__)
--- Flask/env/Scripts/activate.bat
+++ Flask/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\snair\Documents\IOTapp\Flask\env
+set VIRTUAL_ENV=C:\Users\snair\Documents\IOT_Android_Flask\Flask\env
 
 if not defined PROMPT set PROMPT=$P$G
 
